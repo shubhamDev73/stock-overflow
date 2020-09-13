e94384f61c244edbb8a29f394f03f19113276389
Shubham Gupta <shubham0209@gmail.com>
2020-09-13 22:40:39 +0530

    Added player movement script

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public static class GameManager {
+
+	public static float speed {get;} = 7f;
+	public static float smoothness {get;} = 0.9f;
+	public static float tileSize {get;} = 1f;
+
+	public static float ClampToTile (float n, float direction) {
+
+		float result = n / tileSize;
+
+		if(Mathf.Approximately(direction, 0))
+			result = Mathf.Round(result);
+		else if (direction > 0)
+			result = Mathf.Ceil(result);
+		else
+			result = Mathf.Floor(result);
+
+		return tileSize * result;
+
+	}
+
+	public static Vector3 ClampToTile (Vector3 vector, Vector3 direction) {
+		return new Vector3(ClampToTile(vector.x, direction.x), ClampToTile(vector.y, direction.y), ClampToTile(vector.z, direction.z));
+	}
+
+	public static Vector3 ClampToTile (Vector3 vector) {
+		return ClampToTile(vector, Vector3.zero);
+	}
+
+}
---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private Vector3 position;
+
+	void Start () {
+
+		position = transform.position;
+
+	}
+
+	void Update () {
+
+		Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+	}
+
+	void Move (float right, float forward) {
+
+		Vector3 direction = (Vector3.right * right + Vector3.forward * forward).normalized;
+		position += direction * GameManager.speed * Time.deltaTime;
+		Vector3 displacement = GameManager.ClampToTile(position, direction) - transform.position;
+		transform.Translate(displacement * (1 - GameManager.smoothness), Space.World);
+
+	}
+
+}
